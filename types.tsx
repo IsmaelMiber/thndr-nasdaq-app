0de443ac8ef2aa16dde74cbc7eceec7f65083b65
Ismael Miber <ismael.miber@yajny.com>
2022-04-05 01:27:51 +0200

    [ADD] navigation types.

--- types.tsx
+++ types.tsx
@@ -1,8 +1,3 @@
-/**
- * Learn more about using TypeScript with React Navigation:
- * https://reactnavigation.org/docs/typescript/
- */
-
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -14,8 +9,8 @@ declare global {
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
+  Explore: NavigatorScreenParams<ScreensParamList> | undefined;
+  StockDetails: NavigatorScreenParams<ScreensParamList> | undefined,
   NotFound: undefined;
 };
 
@@ -24,12 +19,12 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
   Screen
 >;
 
-export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+export type ScreensParamList = {
+  Explore: undefined;
+  StockDetails: undefined;
 };
 
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
-  BottomTabScreenProps<RootTabParamList, Screen>,
+export type RootTabScreenProps<Screen extends keyof ScreensParamList> = CompositeScreenProps<
+  BottomTabScreenProps<ScreensParamList, Screen>,
   NativeStackScreenProps<RootStackParamList>
 >;
